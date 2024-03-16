3c18c374ec3a8174a69e5e900bb3511fe615491d
BryanTitan <bryan.desola@gmail.com>
2024-03-16 20:46:58 +0100

    [ADD] Mise en place du menu avec 2 boutons
    2 boutons pour afficher soit la carte, soit la liste des capitales

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+
 import { MapComponent } from '../map/map.component';
 import { CapitalsComponent } from '../capitals/capitals.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MapComponent, CapitalsComponent],
+  imports: [MapComponent, CapitalsComponent, NgIf],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  screen: number = 1;
+
+  select(selected: number) {
+    this.screen = selected;
+  }
+}
