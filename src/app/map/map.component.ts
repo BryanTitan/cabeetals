04d76ad76c49bc3656de8787de62aef1d31c6e0d
BryanTitan <bryan.desola@gmail.com>
2024-03-16 21:16:40 +0100

    [ADD] ajout de commentaires

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -9,14 +9,20 @@ import * as L from 'leaflet';
   styleUrl: './map.component.css',
 })
 export class MapComponent implements AfterViewInit {
+  // Afin d'ajouter la carte intéractive, je me suis aidé grâce à la documentation de Leaflet et de l'API OpenStreetMap.
+  // Un tuto est disponible sur le site de DigitalOcean à cette adresse :
+  // https://www.digitalocean.com/community/tutorials/angular-angular-and-leaflet#step-1-setting-up-the-project
   private map: any;
 
+  // On créé une fonction privée distincte appelée initMap() pour isoler toutes les initialisations de la carte.
   private initMap(): void {
+    // Puis on y définit le centre de la carte et la valeur de zoom de départ avec les options de l'API
     this.map = L.map('map', {
-      center: [39.8282, -98.5795],
-      zoom: 3,
+      center: [48.866667, 2.333333], // Paris
+      zoom: 4,
     });
 
+    // On ajoute une couche de tuiles OpenStreetMap à la carte
     const tiles = L.tileLayer(
       'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       {
@@ -32,6 +38,7 @@ export class MapComponent implements AfterViewInit {
 
   constructor() {}
 
+  // Appelé après l’initialisation de la vue du composant
   ngAfterViewInit(): void {
     this.initMap();
   }
