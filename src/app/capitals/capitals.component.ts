8a77a2d2a2aa27e06faa52201703cd7d3944b56f
BryanTitan <bryan.desola@gmail.com>
2024-03-16 19:37:54 +0100

    [ADD] affichage des composants créés

--- src/app/capitals/capitals.component.ts
+++ src/app/capitals/capitals.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-capitals',
+  standalone: true,
+  imports: [],
   templateUrl: './capitals.component.html',
-  styleUrl: './capitals.component.css'
+  styleUrl: './capitals.component.css',
 })
-export class CapitalsComponent {
-
-}
+export class CapitalsComponent {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { MapComponent } from '../map/map.component';
+import { CapitalsComponent } from '../capitals/capitals.component';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [MapComponent, CapitalsComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
