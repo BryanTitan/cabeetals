4231be239ab191f2054da9d25931fc348906e281
BryanTitan <bryan.desola@gmail.com>
2024-03-16 19:32:22 +0100

    [ADD] ajout des 3 composants : home, map, capitals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MapComponent } from './map/map.component';
+import { CapitalsComponent } from './capitals/capitals.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, MapComponent, CapitalsComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/capitals/capitals.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-capitals',
+  templateUrl: './capitals.component.html',
+  styleUrl: './capitals.component.css'
+})
+export class CapitalsComponent {
+
+}
